8dffc9f83f9d3f4170093b0d9c84619dbbd2b209
Mhd Reihan Akmal <akmalsiregar19@gmail.com>
2020-09-18 18:44:09 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("Nama	: M.Reihan Akmal");
+		System.out.println("Nim		: 67062004111");
+		System.out.println("Kelas	: D3if 44-04");
+	}
+}
